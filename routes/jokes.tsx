f755f44ee99828adb8fb49712d65307bc4f22231
kondoumh <kondoumh@gmail.com>
2022-10-20 15:51:12 +0900

    Add random jokes page

---
+++ routes/jokes.tsx
@@ -0,0 +1,21 @@
+// routes/jokes.tsx
+
+/** @jsx h */
+import { h } from "preact";
+import { Handlers } from "$fresh/server.ts";
+
+export const handler: Handlers = {
+  async GET(_, ctx) {
+    const resp = await fetch('./api/joke');
+    const joke = await resp.text();
+    return ctx.render(joke);
+  },
+};
+
+export default function Page({ data }) {
+  return (
+    <div>
+      <h2>{data}</h2>
+    </div>
+  );
+}
